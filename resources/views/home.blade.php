74617ff755fd6ba659c01e5854c3d3aef683dc43
tauficls <tauficleonardo@gmail.com>
2024-07-08 19:28:25 +0700

    adding first laravel learning

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+{{-- @extends('layouts.base') --}}
+<x-base>
+
+<x-slot name="name">
+    Taufic
+</x-slot>
+    {{-- @section('name', 'Taufic') --}}
+
+{{-- @section('content') --}}
+<x-slot name="content">
+<h1 class="text-4xl font-bold">Home</h1>
+<p class="mt-4 text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque in saepe ea eius ex recusandae impedit earum! Aperiam quibusdam quos voluptate quisquam cum saepe incidunt odio delectus! Eius, obcaecati asperiores.</p>
+{{-- @endsection --}}
+</x-slot>
+
+</x-base>
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/home', function () {
+    return view("home", ['name' => "Taufic"]);
+});
+
+Route::get('/about', function () {
+    return view("about");
+});
